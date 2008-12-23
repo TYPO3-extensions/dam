f6f95b5eb1847b4ce1a0bf49e3d253c8d74731df
tuurlijk <tuurlijk@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-23 17:43:40 +0000

    DAM now requires TYPO3 version 4.2.4 because of http://bugs.typo3.org/view.php?id=9511
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam/dam/trunk@15193 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -37,7 +37,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'cms' => '',
 			'static_info_tables' => '',
 			'php' => '4.0.0-',
-			'typo3' => '4.2.2-',
+			'typo3' => '4.2.4-',
 		),
 		'conflicts' => array(
 			'dam_file' => '',
