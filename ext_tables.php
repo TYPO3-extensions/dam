07cce1bdbcda30fb6c0af4905f7fdf847f5e62df
Fabien Udriot <fabien.udriot@ecodev.ch>
2011-09-22 14:19:04 +0700

    [BUGFIX] Add a test to make sure VIDI is available
    Change-Id: I6313d1ed0b208722c1184ec415af2a8ffbf61da3

--- ext_tables.php
+++ ext_tables.php
@@ -40,16 +40,17 @@ if (TYPO3_MODE === 'BE') {
 		)
 	);*/
 
-	/** @var Tx_Vidi_Service_ModuleLoader $moduleLoader */
-	$moduleLoader = t3lib_div::makeInstance('Tx_Vidi_Service_ModuleLoader', $_EXTKEY);
-	$moduleLoader->addStandardTree(Tx_Vidi_Service_ModuleLoader::TREE_FILES);
-	$moduleLoader->setAllowedDataTypes(array('__FILES'));
-	$moduleLoader->setMainModule('file');
-	$moduleLoader->setModuleLanguageFile('LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_dam.xml');
-	$moduleLoader->setIcon('EXT:' . $_EXTKEY . '/ext_icon.gif');
-	$moduleLoader->register();
-
-
+	// Make sure the class exists to avoid a Runtime Error
+	if (class_exists('Tx_Vidi_Service_ModuleLoader')) {
+		/** @var Tx_Vidi_Service_ModuleLoader $moduleLoader */
+		$moduleLoader = t3lib_div::makeInstance('Tx_Vidi_Service_ModuleLoader', $_EXTKEY);
+		$moduleLoader->addStandardTree(Tx_Vidi_Service_ModuleLoader::TREE_FILES);
+		$moduleLoader->setAllowedDataTypes(array('__FILES'));
+		$moduleLoader->setMainModule('file');
+		$moduleLoader->setModuleLanguageFile('LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_dam.xml');
+		$moduleLoader->setIcon('EXT:' . $_EXTKEY . '/ext_icon.gif');
+		$moduleLoader->register();
+	}
 }
 
 
